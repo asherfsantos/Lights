c1b7c433d6187bc101b81f71fc5d5acb3f1c5c89
asherfsantos <40329281+asherfsantos@users.noreply.github.com>
2018-08-07 19:47:00 -0700

    Butterfly follows on trigger

---
+++ Lights/Assets/Scripts/ButterflyScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			transform.parent = other.transform;
+			Vector3 newPosition = new Vector3(other.transform.position.x - 1.5f, other.transform.position.y + 0.5f, other.transform.position.z);
+			transform.position = newPosition;
+		}
+	}
+}
---
+++ Lights/Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour 
+{
+	public GameObject player;
+	public Rigidbody2D playerBody;
+	private float moveInput;
+	public float speed = 10.0f;
+	private bool facingRight = false;
+	public float jumpForce = 5;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.FindWithTag("Player");
+		playerBody = player.GetComponent<Rigidbody2D>();
+	}
+	
+	void FixedUpdate () 
+	{
+		moveInput = Input.GetAxis("Horizontal");
+		playerBody.velocity = new Vector2(moveInput * speed, playerBody.velocity.y);
+		if(!facingRight && moveInput > 0)
+			Flip();
+		else if(facingRight && moveInput < 0)
+			Flip();
+		HandleInput();
+	}
+
+	void Flip()
+	{
+		facingRight = !facingRight;
+		Vector3 Scaler = transform.localScale;
+		Scaler.x *= -1;
+		transform.localScale = Scaler;
+	}
+
+	private void HandleInput()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			playerBody.velocity = Vector2.up * jumpForce;
+		}
+	}
+}
